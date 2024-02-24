76e2cac372460744e855cf8b9c0921620a7715e6
Jillian Schleicher <jmschleicher11@gmail.com>
2024-02-24 05:53:11 -0800

    Cleaned up recipes script, updated tests

--- create_latex.py
+++ create_latex.py
@@ -6,7 +6,7 @@ import os
 import argparse
 
 def frac(a, b):
-    return r'$\\frac{'+str(a)+'}{'+str(b)+'}$'
+    return r"$\\frac{"+str(a)+"}{"+str(b)+"}$"
 
 def clean_fractions(text):
     text = re.sub("¼", frac(1, 4), text)
@@ -18,13 +18,13 @@ def clean_fractions(text):
     text = re.sub("⅔", frac(2, 3), text)
     text = re.sub("²⁄₃", frac(2, 3), text)
     text = re.sub("2( |)/3", frac(2, 3), text)
-    text = re.sub('¾', frac(3, 4), text)
+    text = re.sub("¾", frac(3, 4), text)
     text = re.sub("3( |)/4", frac(3, 4), text)
-    text = re.sub('⅛', frac(1, 8), text)
+    text = re.sub("⅛", frac(1, 8), text)
     text = re.sub("1( |)/8", frac(1, 8), text)
-    text = re.sub('⅜', frac(3, 8), text)
+    text = re.sub("⅜", frac(3, 8), text)
     text = re.sub("3( |)/8", frac(3, 8), text)
-    text = re.sub('⅝', frac(5, 8), text)
+    text = re.sub("⅝", frac(5, 8), text)
     text = re.sub("5( |)/8", frac(5, 8), text)
     return text
 
@@ -48,31 +48,32 @@ def generate_latex(recipe):
     # Set up initial document packages
     geometry_options = {"tmargin": "1cm", "lmargin": "2cm", "rmargin": "2cm", "bmargin": "1cm"}
     doc = Document(fontenc="T1", geometry_options=geometry_options)
-    doc.packages.append(Package('amsmath'))
-    doc.packages.append(Package('nopageno'))
-    doc.packages.append(Package('gensymb'))
-    doc.packages.append(Package('times'))
-    doc.preamble.append(Command('title', recipe.title))
-    doc.preamble.append(Command('author', recipe.source))
-    doc.preamble.append(Command('date', ""))
-    doc.append(NoEscape(r'\maketitle'))
+    doc.packages.append(Package("amsmath"))
+    doc.packages.append(Package("nopageno"))
+    doc.packages.append(Package("gensymb"))
+    doc.packages.append(Package("times"))
+    doc.preamble.append(Command("title", recipe.title))
+    if recipe.source:
+        doc.preamble.append(Command("author", recipe.source))
+    doc.preamble.append(Command("date", ""))
+    doc.append(NoEscape(r"\maketitle"))
 
     # Add image to recipe)
-    with doc.create(Figure(position='h!')) as food_picture:
-        food_picture.add_image(recipe.title + ".png", width='240px')
+    with doc.create(Figure(position="h!")) as food_picture:
+        food_picture.add_image(recipe.title + ".png", width="240px")
 
     # Add recipe timing (active & total)
     if recipe.active_time:
         doc.append(Command("noindent"))
-        doc.append(bold('Active Time: '))
+        doc.append(bold("Active Time: "))
         doc.append(recipe.active_time + " ")
     if recipe.total_time:
         doc.append(Command("noindent"))
-        doc.append(bold('Total Time: '))
+        doc.append(bold("Total Time: "))
         doc.append(recipe.total_time)
 
     # Add list of ingredients
-    with doc.create(Section('Ingredients', numbering=False)):
+    with doc.create(Section("Ingredients", numbering=False)):
         if isinstance(recipe.servings, str):
             doc.append(bold(recipe.servings))
         if isinstance(recipe.ingredients, list):
@@ -89,32 +90,40 @@ def generate_latex(recipe):
 
 
     # Add preparation steps
-    with doc.create(Section('Preparation', numbering=False)):
-        for i in range(len(recipe.steps)):
-            with doc.create(Subsection(recipe.steps[i], numbering=False)):
-                doc.append(NoEscape(create_latex_friendly_text(recipe.instructions[i])))
+    with doc.create(Section("Preparation", numbering=False)):
+        if isinstance(recipe.instructions, list):
+            for i in range(len(recipe.steps)):
+                with doc.create(Subsection(recipe.steps[i], numbering=False)):
+                    doc.append(NoEscape(create_latex_friendly_text(recipe.instructions[i])))
+        else:
+            i = 0
+            for group, instructions_list in recipe.instructions.items():
+                with doc.create(Subsection(group, numbering=False)):
+                    for ins in instructions_list:
+                        with doc.create(Subsection(recipe.steps[i], numbering=False)):
+                            doc.append(NoEscape(create_latex_friendly_text(ins)))
+                        i += 1
 
     # Add optional notes
     if recipe.my_notes:
-        with doc.create(Section('My Notes', numbering=False)):
+        with doc.create(Section("My Notes", numbering=False)):
             doc.append(recipe.my_notes)
 
     # Add source URL
     if recipe.url:
-        with doc.create(Section('Source', numbering=False)):
+        with doc.create(Section("Source", numbering=False)):
             doc.append(recipe.url)
 
     # Generate and save the final pdf document
-    doc.generate_pdf(filepath=str(os.path.join(os.getcwd(), 'pdfs', recipe.title)), clean_tex=True)
+    doc.generate_pdf(filepath=str(os.path.join(os.getcwd(), "pdfs", recipe.title)), clean_tex=True)
 
 
 if __name__ == "__main__":
 
     ## TODO: make graph database of the ingredients?!?!
     ## TODO: extract ingredients list from BA & NYTC
-    ## TODO: add option for grouped ingredients for different components
 
-    parser = argparse.ArgumentParser(description='Enter a recipe URL')
+    parser = argparse.ArgumentParser(description="Enter a recipe URL")
 
     parser.add_argument("--url", type=str, required=False, help="Bon Appetit, NY Times Cooking, or Serious Eats")
     parser.add_argument("--file", type=str, required=False, help="Filename of existing recipe")
@@ -129,10 +138,15 @@ if __name__ == "__main__":
     generate_latex(selected_recipe)
 
     if args.type:
-        if not os.path.exists(os.path.join(os.getcwd(), 'pdfs', args.type)):
-            os.mkdir(os.path.join(os.getcwd(), 'pdfs', args.type))
-        os.rename(os.path.join(os.getcwd(), 'pdfs', selected_recipe.title + '.pdf'),
-                  os.path.join(os.getcwd(), 'pdfs', args.type, selected_recipe.title + '.pdf'))
+        if not os.path.exists(os.path.join(os.getcwd(), "pdfs", args.type)):
+            os.mkdir(os.path.join(os.getcwd(), "pdfs", args.type))
+        os.rename(os.path.join(os.getcwd(), "pdfs", selected_recipe.title + ".pdf"),
+                  os.path.join(os.getcwd(), "pdfs", args.type, selected_recipe.title + ".pdf"))
+    elif selected_recipe.type:
+        if not os.path.exists(os.path.join(os.getcwd(), "pdfs", selected_recipe.type)):
+            os.mkdir(os.path.join(os.getcwd(), "pdfs", selected_recipe.type))
+        os.rename(os.path.join(os.getcwd(), "pdfs", selected_recipe.title + ".pdf"),
+                  os.path.join(os.getcwd(), "pdfs", selected_recipe.type, selected_recipe.title + ".pdf"))
     # Remove the downloaded recipe image
-    os.rename(os.path.join(os.getcwd(), 'pdfs', selected_recipe.title + '.png'),
-              os.path.join(os.getcwd(), 'images', selected_recipe.title + '.png'))
+    os.rename(os.path.join(os.getcwd(), "pdfs", selected_recipe.title + ".png"),
+              os.path.join(os.getcwd(), "images", selected_recipe.title + ".png"))
--- recipes.py
+++ recipes.py
@@ -5,11 +5,11 @@ import re
 import json
 
 def clean_text(text):
-    text = re.sub('\n', ' ', text)
-    text = re.sub('\xa0', '', text)
+    text = re.sub("\n", " ", text)
+    text = re.sub("\xa0", "", text)
     text = text.split("Editor’s note: ")[0]
     text = text.strip()
-    text = re.sub(' {2,}', ' ', text)
+    text = re.sub(" {2,}", " ", text)
     return text
 
 class Recipe:
@@ -17,29 +17,29 @@ class Recipe:
     def __init__(self, url="", file="", source="", type=""):
 
         if file:
-            recipe_dict = json.load(open(os.path.join(os.getcwd(), 'jsons', file + '.json')))
-            self.url = recipe_dict['url']
-            self.source = recipe_dict['source']
-            self.title = recipe_dict['title']
-            self.active_time = recipe_dict['active_time']
-            self.total_time = recipe_dict['total_time']
-            self.servings = recipe_dict['servings']
-            self.ingredients = recipe_dict['ingredients']
-            self.food_list = recipe_dict['food_list']
-            self.steps = recipe_dict['steps']
-            self.instructions = recipe_dict['instructions']
-            if 'my_notes' in recipe_dict.keys():
-                self.my_notes = recipe_dict['my_notes']
+            recipe_dict = json.load(open(os.path.join(os.getcwd(), "jsons", file + ".json")))
+            self.url = recipe_dict["url"]
+            self.source = recipe_dict["source"]
+            self.title = recipe_dict["title"]
+            self.active_time = recipe_dict["active_time"]
+            self.total_time = recipe_dict["total_time"]
+            self.servings = recipe_dict["servings"]
+            self.ingredients = recipe_dict["ingredients"]
+            self.food_list = recipe_dict["food_list"]
+            self.steps = recipe_dict["steps"]
+            self.instructions = recipe_dict["instructions"]
+            if "my_notes" in recipe_dict.keys():
+                self.my_notes = recipe_dict["my_notes"]
             else:
                 self.my_notes = None
-            if 'type' in recipe_dict.keys():
-                self.type = recipe_dict['type']
+            if "type" in recipe_dict.keys():
+                self.type = recipe_dict["type"]
             else:
                 self.type = None
 
             # Transfer file to pdf folder
-            os.rename(os.path.join(os.getcwd(), 'images', self.title + '.png'),
-                      os.path.join(os.getcwd(), 'pdfs', self.title + '.png'))
+            os.rename(os.path.join(os.getcwd(), "images", self.title + ".png"),
+                      os.path.join(os.getcwd(), "pdfs", self.title + ".png"))
 
         else:
             self.url = url
@@ -72,16 +72,16 @@ class Recipe:
         Pull html from website to parse recipe
         """
 
-        if self.source in ['Bon Appetit', "New York Times Cooking", 'Serious Eats']:
+        if self.source in ["Bon Appetit", "New York Times Cooking", "Serious Eats"]:
             page = urlopen(self.url)
             html = page.read().decode("utf-8")
             self.soup = BeautifulSoup(html, "html.parser")
 
-        if self.source == 'Bon Appetit':
+        if self.source == "Bon Appetit":
             self.parse_bon_appetit()
         elif self.source == "New York Times Cooking":
             self.parse_nyt_cooking()
-        elif self.source == 'Serious Eats':
+        elif self.source == "Serious Eats":
             self.parse_serious_eats()
         else:
             self.enter_information_manually()
@@ -90,36 +90,36 @@ class Recipe:
         """
         Extract recipe from bon appetit html
         """
-        self.title = clean_text(self.soup.title.text.split(' | ')[0])
+        self.title = clean_text(self.soup.title.text.split(" | ")[0])
 
         # Get active and total times
-        for i in range(len(self.soup.find_all('p'))):
-            if self.soup.find_all('p')[i].text == 'Active Time':
-                self.active_time = clean_text(self.soup.find_all('p')[i+1].text)
-            elif self.soup.find_all('p')[i].text == 'Total Time':
-                self.total_time = clean_text(self.soup.find_all('p')[i+1].text)
+        for i in range(len(self.soup.find_all("p"))):
+            if self.soup.find_all("p")[i].text == "Active Time":
+                self.active_time = clean_text(self.soup.find_all("p")[i+1].text)
+            elif self.soup.find_all("p")[i].text == "Total Time":
+                self.total_time = clean_text(self.soup.find_all("p")[i+1].text)
 
         # Pull ingredients & preparations tags
         ingredients_tag = None
         preparation_tag = None
 
-        for i in range(len(self.soup.find_all('h2'))):
-            if self.soup.find_all('h2')[i].text == 'Ingredients':
-                ingredients_tag = self.soup.find_all('h2')[i]
-            elif self.soup.find_all('h2')[i].text == 'Preparation':
-                preparation_tag = self.soup.find_all('h2')[i]
+        for i in range(len(self.soup.find_all("h2"))):
+            if self.soup.find_all("h2")[i].text == "Ingredients":
+                ingredients_tag = self.soup.find_all("h2")[i]
+            elif self.soup.find_all("h2")[i].text == "Preparation":
+                preparation_tag = self.soup.find_all("h2")[i]
             else:
                 pass
 
         # Create ingredients list
         if ingredients_tag:
-            if len(ingredients_tag.parent.find_all('p')[0].text) <= 1:
+            if len(ingredients_tag.parent.find_all("p")[0].text) <= 1:
                 pass
             else:
-                self.servings = clean_text(ingredients_tag.parent.find_all('p')[0].text)
-            amounts = ingredients_tag.parent.find_all('p')[1:]
+                self.servings = clean_text(ingredients_tag.parent.find_all("p")[0].text)
+            amounts = ingredients_tag.parent.find_all("p")[1:]
             amounts_list = [i.contents[0] if len(i)>0 else None for i in amounts]
-            ingredients = ingredients_tag.parent.find_all('div')[1:]
+            ingredients = ingredients_tag.parent.find_all("div")[1:]
             ingredients_list = [i.text if len(i)>0 else None for i in ingredients]
             final_ingredients = [amount + " " + ingredient if isinstance(amount, str) else ingredient
                                  for amount, ingredient in zip(amounts_list, ingredients_list)]
@@ -128,8 +128,8 @@ class Recipe:
 
         # Pull preparation steps
         if preparation_tag:
-            step_numbers = [item.contents[0] for item in preparation_tag.parent.find_all('h4')]
-            instructions = [item.text for item in preparation_tag.parent.find_all('p')]
+            step_numbers = [item.contents[0] for item in preparation_tag.parent.find_all("h4")]
+            instructions = [item.text for item in preparation_tag.parent.find_all("p")]
             if len(step_numbers) != len(instructions):
                 for i, inst in enumerate(instructions):
                     if re.match("Do ahead: ", inst, flags=re.IGNORECASE):
@@ -153,26 +153,26 @@ class Recipe:
             self.instructions = instructions
 
         # Pull image & save temporarily
-        image_url = self.soup.find("source", {"media": "(max-width: 767px)"})['srcset'].split(' ')[-2]
-        urlretrieve(image_url, filename=os.path.join(os.getcwd(), 'pdfs', self.title + '.png'))
+        image_url = self.soup.find("source", {"media": "(max-width: 767px)"})["srcset"].split(" ")[-2]
+        urlretrieve(image_url, filename=os.path.join(os.getcwd(), "pdfs", self.title + ".png"))
 
     def parse_nyt_cooking(self):
         """
         Extract recipe from nytimes cooking html
         """
-        self.title = self.soup.title.text.split(' - ')[0].replace(" Recipe", "")
+        self.title = self.soup.title.text.split(" - ")[0].replace(" Recipe", "")
 
         self.total_time = self.soup.find_all("dd", class_="pantry--ui")[0].text
 
         # Pull number of servings
-        servings_tag = self.soup.find('div', class_=re.compile('ingredients_recipeYield_*'))
-        servings_text = servings_tag.find_all('span',
-                                              class_=re.compile('pantry--ui ingredients_fontOverride_*'))[0].text
-        if re.search(r'\d+$', servings_text):
-            if re.search('serv(es|ings)', servings_text, re.IGNORECASE):
+        servings_tag = self.soup.find("div", class_=re.compile("ingredients_recipeYield_*"))
+        servings_text = servings_tag.find_all("span",
+                                              class_=re.compile("pantry--ui ingredients_fontOverride_*"))[0].text
+        if re.search(r"\d+$", servings_text):
+            if re.search("serv(es|ings)", servings_text, re.IGNORECASE):
                 self.servings = servings_text
             else:
-                self.servings = servings_text + ' Servings'
+                self.servings = servings_text + " Servings"
         else:
             self.servings = servings_text
 
@@ -180,11 +180,11 @@ class Recipe:
         ingredients_list = self.soup.find("div", class_=re.compile("recipebody_ingredients-block_*")).find_all("li")
         ingredients = []
         for ingredient in ingredients_list:
-            span = ingredient.find('span', class_=re.compile('ingredient_quantity_*'))
+            span = ingredient.find("span", class_=re.compile("ingredient_quantity_*"))
             if span:
                 quantity = span.text
                 ingredient_name = ingredient.text.strip()[len(quantity):]
-                ingredients.append(f'{quantity} {ingredient_name}')
+                ingredients.append(f"{quantity} {ingredient_name}")
             else:
                 ingredients.append(ingredient.text.strip())
 
@@ -209,8 +209,8 @@ class Recipe:
         self.instructions = instructions
 
         # Pull image & save temporarily
-        image_url = self.soup.find("img")['src']
-        urlretrieve(image_url, filename=os.path.join(os.getcwd(), 'pdfs', self.title + '.png'))
+        image_url = self.soup.find("img")["src"]
+        urlretrieve(image_url, filename=os.path.join(os.getcwd(), "pdfs", self.title + ".png"))
 
     def parse_serious_eats(self):
 
@@ -230,7 +230,7 @@ class Recipe:
         ingredients_list = self.soup.find("ul", class_="structured-ingredients__list text-passage").find_all("p")
         for ingredient in ingredients_list:
             ingredients.append(ingredient.text)
-            food_list.append(ingredient.find("span", {"data-ingredient-name": 'true'}).text.lower())
+            food_list.append(ingredient.find("span", {"data-ingredient-name": "true"}).text.lower())
 
         self.ingredients = ingredients
         self.food_list = food_list
@@ -238,8 +238,8 @@ class Recipe:
         instructions = []
         step_numbers = []
         step_index = 1
-        instructions_tag = self.soup.find('ol', class_="comp mntl-sc-block-group--OL mntl-sc-block mntl-sc-block-startgroup")
-        instruction_list = instructions_tag.find_all('p', class_="comp mntl-sc-block mntl-sc-block-html")
+        instructions_tag = self.soup.find("ol", class_="comp mntl-sc-block-group--OL mntl-sc-block mntl-sc-block-startgroup")
+        instruction_list = instructions_tag.find_all("p", class_="comp mntl-sc-block mntl-sc-block-html")
         for instruction in instruction_list:
             instructions.append(clean_text(instruction.text))
             step_numbers.append("Step {}".format(step_index))
@@ -253,14 +253,14 @@ class Recipe:
             if re.search("Notes", item.text):
                 notes_tag = item
         if notes_tag:
-            split_id = notes_tag['id'].split('-')
+            split_id = notes_tag["id"].split("-")
             notes_id = int(split_id[-1]) + 1
-            notes_text_id = '-'.join(split_id[:-1] + [str(notes_id)])
-            notes_text = ''
+            notes_text_id = "-".join(split_id[:-1] + [str(notes_id)])
+            notes_text = ""
             while self.soup.find("p", id=notes_text_id):
                 notes_text += " " + self.soup.find("p", id=notes_text_id).text.strip()
                 notes_id += 2
-                notes_text_id = '-'.join(split_id[:-1] + [str(notes_id)])
+                notes_text_id = "-".join(split_id[:-1] + [str(notes_id)])
 
             instructions.append(clean_text(notes_text))
             step_numbers.append("Notes")
@@ -270,61 +270,72 @@ class Recipe:
 
         # Pull image & save temporarily
         try:
-            image_url = self.soup.find("figure").find("img")['src']
+            image_url = self.soup.find("figure").find("img")["src"]
         except:
-            image_url = self.soup.find("figure").find("img")['data-src']
-        urlretrieve(image_url, filename=os.path.join(os.getcwd(), 'pdfs', self.title + '.png'))
+            image_url = self.soup.find("figure").find("img")["data-src"]
+        urlretrieve(image_url, filename=os.path.join(os.getcwd(), "pdfs", self.title + ".png"))
 
     def enter_information_manually(self):
 
         self.title = input("Enter title: ")
+        if not self.source:
+            self.source = input("Enter source: ")
         self.active_time = input("Enter active time: ")
         self.total_time = input("Enter total time: ")
         self.servings = input("Enter number of servings: ")
+
+        if os.path.exists(os.path.join(os.getcwd(), "images", self.title + ".png")):
+            os.rename(os.path.join(os.getcwd(), "images", self.title + ".png"),
+                      os.path.join(os.getcwd(), "pdfs", self.title + ".png"))
+        else:
+            print("Save an image of the recipe in the pdf directory")
 
         self.ingredients = collect_list_of_things("ingredients")
 
         self.food_list = None
 
-        num_steps = input("Enter the number of steps: ")
-        steps_list = ['Step ' + str(i) for i in list(range(1, int(num_steps) + 1))]
-        do_ahead = input("Do ahead instructions? (y/n): ")
-        if do_ahead == 'y':
-            steps_list.append("Do ahead")
-
-        self.steps = steps_list
-
         instruction_list = collect_list_of_things("instructions")
 
-        if len(steps_list) != len(instruction_list):
-            print("There are not the same number of steps and instructions")
+        do_ahead = input("Do ahead instructions? (y/n): ")
+        if do_ahead == "y":
+            steps_list = ["Step " + str(i) for i in list(range(1, len(instruction_list)))]
+            steps_list.append("Do ahead")
+        else:
+            steps_list = ["Step " + str(i) for i in list(range(1, len(instruction_list) + 1))]
 
+        self.steps = steps_list
         self.instructions = instruction_list
 
+        if os.path.exists(os.path.join(os.getcwd(), "images", self.title + ".png")):
+            os.rename(os.path.join(os.getcwd(), "images", self.title + ".png"),
+                      os.path.join(os.getcwd(), "pdfs", self.title + ".png"))
+        else:
+            print("Save an image of the recipe in the pdf directory")
+
     def to_json(self):
-        if os.path.isfile(os.path.join(os.getcwd(), 'jsons', self.title + '.json')):
+        if os.path.isfile(os.path.join(os.getcwd(), "jsons", self.title + ".json")):
             user_says = input("This json file already exists. Overwrite? (y/n): ")
-            if user_says == 'y':
+            if user_says == "y":
                 recipe_dict = vars(self)
-                if 'soup' in recipe_dict.keys():
-                    recipe_dict.pop('soup')
-                with open(os.path.join(os.getcwd(), 'jsons', self.title + '.json'), 'w', encoding='utf-8') as f:
+                if "soup" in recipe_dict.keys():
+                    recipe_dict.pop("soup")
+                with open(os.path.join(os.getcwd(), "jsons", self.title + ".json"), "w", encoding="utf-8") as f:
                     json.dump(recipe_dict, f, ensure_ascii=False, indent=4)
             else:
                 print("Did not overwrite the json file.")
         else:
             recipe_dict = vars(self)
-            if 'soup' in recipe_dict.keys():
-                recipe_dict.pop('soup')
-            with open(os.path.join(os.getcwd(), 'jsons', self.title + '.json'), 'w', encoding='utf-8') as f:
+            if "soup" in recipe_dict.keys():
+                recipe_dict.pop("soup")
+            with open(os.path.join(os.getcwd(), "jsons", self.title + ".json"), "w", encoding="utf-8") as f:
                 json.dump(recipe_dict, f, ensure_ascii=False, indent=4)
 
 
 
 def collect_list_of_things(type_of_thing):
-    more = 'y'
+    more = "y"
     thing_list = []
-    while more != 'n':
+    while more != "n":
         thing_list.append(input("Enter {}: ".format(type_of_thing)))
         more = input("More {}? (y/n)".format(type_of_thing))
     return thing_list
